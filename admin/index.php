4dd179a1c4e84d24b339c133dc84653993551815
nurnaby <sohag.biit@gmail.com>
2022-05-24 13:10:08 +0600

    create login page

--- admin/index.php
+++ admin/index.php
@@ -210,16 +210,7 @@
             <?php include'part/footer.php'; ?>
         </div>
     </div>
-    <!-- General JS Scripts -->
-    <script src="assets/js/app.min.js"></script>
-    <!-- JS Libraies -->
-    <script src="assets/bundles/apexcharts/apexcharts.min.js"></script>
-    <!-- Page Specific JS File -->
-    <script src="assets/js/page/index.js"></script>
-    <!-- Template JS File -->
-    <script src="assets/js/scripts.js"></script>
-    <!-- Custom JS File -->
-    <script src="assets/js/custom.js"></script>
+    <?php include'part/js-link.php';?>
 </body>
 
 
--- admin/part/header.php
+++ admin/part/header.php
@@ -4,6 +4,8 @@
     <title>Admin Dashboard </title>
     <!-- General CSS Files -->
     <link rel="stylesheet" href="assets/css/app.min.css">
+    <link rel="stylesheet" href="assets/bundles/bootstrap-social/bootstrap-social.css">
+
     <!-- Template CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/components.css">
